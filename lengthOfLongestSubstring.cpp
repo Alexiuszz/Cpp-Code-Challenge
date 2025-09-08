c3091779f597c588aab9865324b73b83095a7068
Nwala Enyinna <65119080+Alexiuszz@users.noreply.github.com>
2025-09-08 22:54:14 +0100

    Update lengthOfLongestSubstring.cpp

--- lengthOfLongestSubstring.cpp
+++ lengthOfLongestSubstring.cpp
@@ -8,16 +8,18 @@ class Solution
 public:
         int lengthOfLongestSubstring(string s)
         {
-                int maxLength = 0, start = 0;
-                int chars[128];
-                std::fill_n(chars, 128, -1);
+                int maxLength = 0, start = 0; // start of the current substring without repeating characters
+                int chars[128]; // ASCII character set
+                std::fill_n(chars, 128, -1);  // Initialize all characters as not seen
 
-                for (int end = 0; end < s.length(); ++end)
+                for (int end = 0; end < s.length(); ++end) // end is the end of the current substring, 
                 {
-                        const int charCode = static_cast<int>(s[end]);
+                        const int charCode = static_cast<int>(s[end]); // Get ASCII value of the character
+                        // If the character was seen and is within the current substring
                         start = start > chars[charCode] + 1 ? start : chars[charCode] + 1;
-                        chars[charCode] = end;
-                        maxLength = maxLength > end - start + 1 ? maxLength : end - start + 1;
+                        chars[charCode] = end;// Update the last seen index of the character
+                        
+                        maxLength = maxLength > end - start + 1 ? maxLength : end - start + 1;// Update maxLength if we found a longer substring
                 }
                 return maxLength;
         }
