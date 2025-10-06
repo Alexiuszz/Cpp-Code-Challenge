dff87f11191e955eb90bbe6ddf0d57486397da2d
Nwala Enyinna <65119080+Alexiuszz@users.noreply.github.com>
2025-10-06 20:37:08 +0100

    heap sort

--- heap.cpp
+++ heap.cpp
@@ -11,13 +11,47 @@ class Heap
 private:
     std::vector<int> h;
     void MaxHeapifyRec(std::vector<int> &a, int i, int heapSize);
-    void MaxHeapify(std::vector<int> &a, int i);
+    void MaxHeapify(std::vector<int> &a, int i, int heapSize);
     void BuildHeap(std::vector<int> &a);
 
 public:
     Heap();
-    Heap(std::vector<int> *h);
+    Heap(std::vector<int> &h);
+
+    int GetLeft(int i);
+    int GetRight(int i);
 };
+
+Heap::Heap(std::vector<int> &h)
+{
+    BuildHeap(h);
+}
+
+int Heap::GetLeft(int i)
+{
+    return 2 * i + 1;
+}
+int Heap::GetRight(int i)
+{
+    return 2 * i + 2;
+}
+
+void Heap::MaxHeapify(std::vector<int> &a, int i, int heapSize)
+{
+    while (true)
+    {
+        int largest = i;
+        int l = 2 * i + 1, r = 2 * i + 2;
+        if (l < heapSize && a[l] > a[largest])
+            largest = l;
+        if (r < heapSize && a[r] > a[largest])
+            largest = r;
+        if (largest == i)
+            return;
+        std::swap(a[largest], a[i]);
+        i = largest;
+    }
+}
 
 void Heap::MaxHeapifyRec(std::vector<int> &a, int i, int heapSize)
 {
@@ -39,6 +73,17 @@ void Heap::BuildHeap(std::vector<int> &a)
     int heapSize = static_cast<int>(a.size());
     for (int i = heapSize / 2 - 1; i >= 0; --i)
     {
-        MaxHeapifyRec(a, i, heapSize);
+        MaxHeapify(a, i, heapSize);
+    }
+}
+
+int main()
+{
+    std::vector<int> arr = {2, 1, 4, 5};
+    Heap h(arr);
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        std::cout << arr[i] << " ";
     }
 }
